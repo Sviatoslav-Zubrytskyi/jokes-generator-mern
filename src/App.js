f8d3e0b5a9193f168786e800efd6187b235d7df3
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-11-24 07:07:50 +0100

    jokes generator + mern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [joke, setJoke] = useState('');
+    const [jokes, setJokes] = useState([]);
+
+    useEffect(() => {
+        async function getJokes() {
+            const response = await fetch(`http://localhost:5000/record/`);
+
+            if (!response.ok) {
+                const message = `An error occurred: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
+
+            const jokes = await response.json();
+            setJokes(jokes);
+        }
+
+        getJokes();
+
+        return;
+    }, []);
+    const getRandomJoke = () => {
+        if (jokes.length === 0) setJoke("No jokes left. Come tomorrow")
+        else setJoke(jokes.splice(Math.floor(Math.random() * jokes.length), 1))
+    }
+
+    return (
+        <div className={"body"}>
+            <button className="joke-button" id={"jokeButton"} onClick={getRandomJoke}>Generate random joke</button>
+            <h1 className="joke">{joke}</h1>
+        </div>
+    );
 }
 
 export default App;
